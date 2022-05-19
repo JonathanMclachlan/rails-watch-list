fccafafb2c63456238036185c39ba23341912220
Jonathan McLachlan <jonathan.mclachlan@hotmail.com>
2022-05-19 17:09:02 +0200

    pre live code on thursday

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'json'
+
+puts "deleting existing data"
+Movie.destroy_all
+
+puts "fetching data"
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+movie_data_serialized = URI.open(url).read
+movie_data = JSON.parse(movie_data_serialized)
+base_image_url = "https://image.tmdb.org/t/p/w500"
+
+puts "creating movies"
+
+movie_data["results"].each do |movie_hash|
+  Movie.create(
+    title: movie_hash["title"],
+    overview: movie_hash["overview"],
+    poster_url: "#{base_image_url}#{movie_hash["poster_path"]}",
+    rating: movie_hash["vote_average"]
+  )
+end
+
+puts "done creating movies"
